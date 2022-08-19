df52eddf34f175b390a72200deb52f47d31cf69c
Umesh Yadav <umeshyadav@umeshs-mbp.lan>
2022-08-18 21:38:09 -0400

    Remove cloud block

--- versions.tf
+++ versions.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  cloud {
-    organization = "hashicorp-learn"
+  # cloud {
+  #   organization = "TNC"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform-cloud"
+  #   }
+  # }
 
   required_providers {
     aws = {
